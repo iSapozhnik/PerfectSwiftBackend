c2908621de554ff1b91898134343d9239a281ab0
Sapozhnik Ivan <sapozhnik.ivan@gmail.com>
2016-06-27 02:45:28 +0300

    Perfect framework added

---
+++ HelloPerfect/HelloPerfect/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HelloPerfect
+//
+//  Created by Sapozhnik Ivan on 27.06.16.
+//  Copyright © 2016 Sapozhnik Ivan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
